8520ab98e07565c6157993021ee4a13fc3c6ce41
min0962 <wsx8028@gmail.com>
2025-06-11 17:29:27 +0900

    ApprovalStep 도메인 메서드 테스트 작성

---
+++ backend/team6/src/test/java/programmers/team6/domain/vacation/entity/ApprovalStepTests.java
@@ -0,0 +1,137 @@
+package programmers.team6.domain.vacation.entity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static programmers.team6.domain.vacation.entity.util.ApprovalStepTestUtils.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import programmers.team6.domain.vacation.enums.ApprovalStatus;
+import programmers.team6.global.exception.customException.BadRequestException;
+
+class ApprovalStepTests {
+
+	@Test
+	@DisplayName("사유 업데이트시 정상적으로 변경된다")
+	void update_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.PENDING);
+		String reason = "~~해서 안됩니다.";
+
+		approvalStep.update(reason);
+
+		assertThat(approvalStep.getReason()).isEqualTo(reason);
+	}
+
+	@Test
+	@DisplayName("승인 처리시 상태가 APPROVED로 변경된다")
+	void approve_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.PENDING);
+
+		approvalStep.approve();
+
+		assertThat(approvalStep.getApprovalStatus()).isEqualTo(ApprovalStatus.APPROVED);
+	}
+
+	@Test
+	@DisplayName("사유와 함께 반려 처리시 상태가 REJECTED로 변경된다")
+	void reject_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.PENDING);
+		String reason = "~~해서 안됩니다.";
+
+		approvalStep.reject(reason);
+
+		assertThat(approvalStep.getApprovalStatus()).isEqualTo(ApprovalStatus.REJECTED);
+	}
+
+	@Test
+	@DisplayName("대기 처리시 상태가 PENDING 변경된다")
+	void pending_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.WAITING);
+
+		approvalStep.pending();
+
+		assertThat(approvalStep.getApprovalStatus()).isEqualTo(ApprovalStatus.PENDING);
+	}
+
+	@Test
+	@DisplayName("취소 처리시 상태가 CANCELED로 변경된다")
+	void calcel_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.CANCELED);
+
+		approvalStep.cancel();
+
+		assertThat(approvalStep.getApprovalStatus()).isEqualTo(ApprovalStatus.CANCELED);
+	}
+
+	@Test
+	@DisplayName("상태가 PENDING이면 승인검증 시 예외가 발생하지 않을 것이다.")
+	void approvable_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.PENDING);
+
+		assertDoesNotThrow(approvalStep::validateApprovable);
+	}
+
+	@Test
+	@DisplayName("상태가 PENDING이 아니면 승인검증 시 예외가 발생할 것이다.")
+	void un_approvable_test() throws Exception {
+		ApprovalStep approvalStep1 = genApprovalStep(ApprovalStatus.APPROVED);
+		ApprovalStep approvalStep2 = genApprovalStep(ApprovalStatus.REJECTED);
+		ApprovalStep approvalStep3 = genApprovalStep(ApprovalStatus.WAITING);
+		ApprovalStep approvalStep4 = genApprovalStep(ApprovalStatus.CANCELED);
+
+		assertThrows(
+			BadRequestException.class,
+			approvalStep1::validateApprovable
+		);
+		assertThrows(
+			BadRequestException.class,
+			approvalStep2::validateApprovable
+		);
+		assertThrows(
+			BadRequestException.class,
+			approvalStep3::validateApprovable
+		);
+		assertThrows(
+			BadRequestException.class,
+			approvalStep4::validateApprovable
+		);
+
+	}
+
+	@Test
+	@DisplayName("상태가 PENDING이면 반려검증 시 예외가 발생하지 않을 것이다.")
+	void rejectable_test() throws Exception {
+		ApprovalStep approvalStep = genApprovalStep(ApprovalStatus.PENDING);
+
+		assertDoesNotThrow(approvalStep::validateRejectable);
+	}
+
+	@Test
+	@DisplayName("상태가 PENDING이 아니면 반려검증 시 예외가 발생할 것이다.")
+	void un_rejectable_test() throws Exception {
+		ApprovalStep approvalStep1 = genApprovalStep(ApprovalStatus.APPROVED);
+		ApprovalStep approvalStep2 = genApprovalStep(ApprovalStatus.REJECTED);
+		ApprovalStep approvalStep3 = genApprovalStep(ApprovalStatus.WAITING);
+		ApprovalStep approvalStep4 = genApprovalStep(ApprovalStatus.CANCELED);
+
+		assertThrows(
+			BadRequestException.class,
+			approvalStep1::validateRejectable
+		);
+		assertThrows(
+			BadRequestException.class,
+			approvalStep2::validateRejectable
+		);
+		assertThrows(
+			BadRequestException.class,
+			approvalStep3::validateRejectable
+		);
+		assertThrows(
+			BadRequestException.class,
+			approvalStep4::validateRejectable
+		);
+
+	}
+
+}
