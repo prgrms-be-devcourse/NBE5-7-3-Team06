7bc96a1cd2bfb3a24356d88d0b68e84fef02612e
gunwoong <gunwoong1630@gmail.com>
2025-06-12 16:58:16 +0900

    test: VacationService 테스트 추가

---
+++ backend/team6/src/test/java/programmers/team6/domain/vacation/service/VacationServiceIntegrationTest.java
@@ -0,0 +1,222 @@
+package programmers.team6.domain.vacation.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static programmers.team6.domain.admin.repository.AdminVacationRequestSearchTestDataFactory.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import programmers.team6.domain.member.entity.Code;
+import programmers.team6.domain.member.entity.Dept;
+import programmers.team6.domain.member.entity.Member;
+import programmers.team6.domain.member.repository.CodeRepository;
+import programmers.team6.domain.member.repository.DeptRepository;
+import programmers.team6.domain.member.repository.MemberRepository;
+import programmers.team6.domain.member.service.DeptService;
+import programmers.team6.domain.vacation.dto.VacationCreateRequestDto;
+import programmers.team6.domain.vacation.dto.VacationCreateResponseDto;
+import programmers.team6.domain.vacation.dto.VacationInfoSelectResponseDto;
+import programmers.team6.domain.vacation.dto.VacationRequestCalendarResponse;
+import programmers.team6.domain.vacation.dto.VacationUpdateRequestDto;
+import programmers.team6.domain.vacation.dto.VacationUpdateResponseDto;
+import programmers.team6.domain.vacation.entity.VacationInfo;
+import programmers.team6.domain.vacation.entity.VacationRequest;
+import programmers.team6.domain.vacation.enums.VacationCode;
+import programmers.team6.domain.vacation.enums.VacationRequestStatus;
+import programmers.team6.domain.vacation.repository.VacationInfoRepository;
+import programmers.team6.domain.vacation.repository.VacationRequestRepository;
+import programmers.team6.domain.vacation.repository.VacationRequestSearchRepository;
+import programmers.team6.domain.vacation.util.mapper.VacationMapper;
+
+/**
+ * VacationService의 코드가 리팩토링이 많이 필요해보임, 리팩토링후 해당 테스트는 쉽게 교체될것같기에 임시로 테스트를 작성하였음
+ * @author gunwoong
+ */
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({VacationService.class, VacationMapper.class, ApprovalStepService.class, VacationInfoLogPublisher.class,
+	DeptService.class, VacationRequestSearchRepository.class})
+class VacationServiceIntegrationTest {
+	@Autowired
+	private VacationService vacationService;
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private CodeRepository codeRepository;
+	@Autowired
+	private DeptRepository deptRepository;
+	@Autowired
+	private VacationInfoRepository vacationInfoRepository;
+	@Autowired
+	private VacationRequestRepository vacationRequestRepository;
+
+	@Test
+	void should_findMyVacationInfo_when_givenMemberId() {
+		// given & when
+		Code positionCode = codeRepository.save(genTestCode("POSITION", "00", "name"));
+		Dept dept = deptRepository.save(genTestDept("deptName"));
+		Member requester = memberRepository.save(genTestMember("A", dept, positionCode));
+		vacationInfoRepository.save(new VacationInfo(100d, 5d, VacationCode.ANNUAL.getCode(), requester.getId()));
+
+		// then
+		VacationInfoSelectResponseDto response = vacationService.getMyVacationInfo(requester.getId());
+		assertThat(response).extracting(VacationInfoSelectResponseDto::getTotalCount,
+				VacationInfoSelectResponseDto::getUseCount, VacationInfoSelectResponseDto::getRemainCount)
+			.containsExactly(100d, 5d, 100d - 5d);
+	}
+
+	@Test
+	void should_saveVacation_when_givenValidMemberIdAndVacationCreateRequestDto() {
+		// given & when
+		Code vacationTypeCode = codeRepository.save(genTestCode("VACATION_TYPE", "00", "name"));
+		Code positionCode = codeRepository.save(genTestCode("POSITION", "00", "name"));
+
+		Dept dept = deptRepository.save(genTestDept("deptName"));
+		Dept hrDept = deptRepository.save(genTestDept("인사팀"));
+
+		Member requester = memberRepository.save(genTestMember("A", dept, positionCode));
+		Member firstApprover = memberRepository.save(genTestMember("B", dept, positionCode));
+		Member secondApprover = memberRepository.save(genTestMember("C", hrDept, positionCode));
+		dept.appointLeader(firstApprover);
+		hrDept.appointLeader(secondApprover);
+
+		vacationInfoRepository.save(new VacationInfo(100d, 5d, vacationTypeCode.getCode(), requester.getId()));
+
+		LocalDateTime from = LocalDateTime.now().plusDays(1);
+		LocalDateTime to = from.plusDays(1);
+		VacationCreateRequestDto vacationCreateRequestDto = VacationCreateRequestDto.builder()
+			.from(from)
+			.to(to)
+			.reason("reason")
+			.vacationType(vacationTypeCode.getCode())
+			.build();
+
+		// then
+		VacationCreateResponseDto response = vacationService.requestVacation(requester.getId(),
+			vacationCreateRequestDto);
+		assertThat(response)
+			.extracting(VacationCreateResponseDto::getFrom, VacationCreateResponseDto::getTo)
+			.containsExactly(from, to);
+	}
+
+	@Test
+	void should_updateVacation_when_givenValidMemberIdAndVacationRequestIdAndVacationUpdateRequestDto() {
+		// given & when
+		Code vacationTypeCode = codeRepository.save(genTestCode("VACATION_TYPE", "00", "name"));
+		Code positionCode = codeRepository.save(genTestCode("POSITION", "00", "name"));
+
+		Dept dept = deptRepository.save(genTestDept("deptName"));
+		Dept hrDept = deptRepository.save(genTestDept("인사팀"));
+
+		Member requester = memberRepository.save(genTestMember("A", dept, positionCode));
+		Member firstApprover = memberRepository.save(genTestMember("B", dept, positionCode));
+		Member secondApprover = memberRepository.save(genTestMember("C", hrDept, positionCode));
+		dept.appointLeader(firstApprover);
+		hrDept.appointLeader(secondApprover);
+
+		vacationInfoRepository.save(new VacationInfo(100d, 5d, vacationTypeCode.getCode(), requester.getId()));
+
+		LocalDateTime from = LocalDateTime.now().plusDays(1);
+		LocalDateTime to = from.plusDays(1);
+
+		VacationCreateResponseDto response = vacationService.requestVacation(requester.getId(),
+			VacationCreateRequestDto.builder()
+				.from(from)
+				.to(to)
+				.reason("reason")
+				.vacationType(vacationTypeCode.getCode())
+				.build());
+
+		LocalDateTime updatedFrom = LocalDateTime.now().plusDays(5);
+		LocalDateTime updatedTo = updatedFrom.plusDays(1);
+		VacationUpdateRequestDto updateRequestDto = new VacationUpdateRequestDto(updatedFrom, updatedTo,
+			"updatedReason",
+			vacationTypeCode.getCode());
+
+		// then
+		VacationUpdateResponseDto result = vacationService.updateVacationRequest(requester.getId(),
+			response.getRequestId(), updateRequestDto);
+
+		assertThat(result)
+			.extracting(VacationUpdateResponseDto::getRequestId, VacationUpdateResponseDto::getFrom,
+				VacationUpdateResponseDto::getTo)
+			.containsExactly(response.getRequestId(), updatedFrom, updatedTo);
+	}
+
+	@Test
+	void should_cancelVacationRequest_when_givenMemberIdAndRequestId() {
+		// given & when
+		Code vacationTypeCode = codeRepository.save(genTestCode("VACATION_TYPE", "00", "name"));
+		Code positionCode = codeRepository.save(genTestCode("POSITION", "00", "name"));
+
+		Dept dept = deptRepository.save(genTestDept("deptName"));
+		Dept hrDept = deptRepository.save(genTestDept("인사팀"));
+
+		Member requester = memberRepository.save(genTestMember("A", dept, positionCode));
+		Member firstApprover = memberRepository.save(genTestMember("B", dept, positionCode));
+		Member secondApprover = memberRepository.save(genTestMember("C", hrDept, positionCode));
+		dept.appointLeader(firstApprover);
+		hrDept.appointLeader(secondApprover);
+
+		vacationInfoRepository.save(new VacationInfo(100d, 5d, vacationTypeCode.getCode(), requester.getId()));
+
+		LocalDateTime from = LocalDateTime.now().plusDays(1);
+		LocalDateTime to = from.plusDays(1);
+		VacationCreateRequestDto vacationCreateRequestDto = VacationCreateRequestDto.builder()
+			.from(from)
+			.to(to)
+			.reason("reason")
+			.vacationType(vacationTypeCode.getCode())
+			.build();
+		VacationCreateResponseDto response = vacationService.requestVacation(requester.getId(),
+			vacationCreateRequestDto);
+
+		// then
+		vacationService.cancelVacationRequest(requester.getId(), response.getRequestId());
+		Optional<VacationRequest> result = vacationRequestRepository.findById(response.getRequestId());
+		assertThat(result).isPresent();
+		assertThat(result.get().getStatus()).isEqualTo(VacationRequestStatus.CANCELED);
+	}
+
+	@Test
+	void should_selectVacationCalendar_when_givenYearAndDeptId() {
+		// given & when
+		Code vacationTypeCode = codeRepository.save(genTestCode("VACATION_TYPE", "00", "name"));
+		Code positionCode = codeRepository.save(genTestCode("POSITION", "00", "name"));
+
+		Dept dept = deptRepository.save(genTestDept("deptName"));
+		Dept hrDept = deptRepository.save(genTestDept("인사팀"));
+
+		Member requester = memberRepository.save(genTestMember("A", dept, positionCode));
+		Member firstApprover = memberRepository.save(genTestMember("B", dept, positionCode));
+		Member secondApprover = memberRepository.save(genTestMember("C", hrDept, positionCode));
+		dept.appointLeader(firstApprover);
+		hrDept.appointLeader(secondApprover);
+
+		vacationInfoRepository.save(new VacationInfo(100d, 5d, vacationTypeCode.getCode(), requester.getId()));
+
+		LocalDateTime from = LocalDateTime.of(2025, 1, 1, 0, 0, 0);
+		LocalDateTime to = from.plusDays(1);
+		VacationCreateRequestDto vacationCreateRequestDto = VacationCreateRequestDto.builder()
+			.from(from)
+			.to(to)
+			.reason("reason")
+			.vacationType(vacationTypeCode.getCode())
+			.build();
+		VacationCreateResponseDto response = vacationService.requestVacation(requester.getId(),
+			vacationCreateRequestDto);
+		vacationRequestRepository.findById(response.getRequestId()).ifPresent(result -> result.approve());
+
+		// then
+		List<VacationRequestCalendarResponse> result = vacationService.selectVacationCalendar(
+			"2025-01", dept.getId());
+		assertThat(result).hasSize(1);
+	}
+}
--- backend/team6/src/test/java/programmers/team6/domain/vacation/service/VacationServiceTests.java
+++ backend/team6/src/test/java/programmers/team6/domain/vacation/service/VacationServiceTests.java
@@ -1,83 +1,229 @@
 package programmers.team6.domain.vacation.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Optional;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import lombok.extern.slf4j.Slf4j;
-import programmers.team6.domain.auth.dto.request.MemberSignUpRequest;
-import programmers.team6.domain.auth.service.AuthService;
-import programmers.team6.domain.member.entity.Code;
 import programmers.team6.domain.member.entity.Member;
 import programmers.team6.domain.member.repository.CodeRepository;
 import programmers.team6.domain.member.repository.MemberRepository;
-import programmers.team6.domain.vacation.dto.VacationRequestCalendarResponse;
-import programmers.team6.domain.vacation.entity.VacationRequest;
-import programmers.team6.domain.vacation.enums.VacationRequestStatus;
+import programmers.team6.domain.vacation.dto.VacationCreateRequestDto;
+import programmers.team6.domain.vacation.dto.VacationListResponseDto;
+import programmers.team6.domain.vacation.repository.ApprovalStepRepository;
+import programmers.team6.domain.vacation.repository.VacationInfoRepository;
 import programmers.team6.domain.vacation.repository.VacationRequestRepository;
+import programmers.team6.domain.vacation.repository.VacationRequestSearchRepository;
+import programmers.team6.domain.vacation.util.mapper.VacationMapper;
+import programmers.team6.global.exception.customException.BadRequestException;
+import programmers.team6.global.exception.customException.NotFoundException;
+import programmers.team6.support.MemberMother;
 
-@Slf4j
-@SpringBootTest
-@Transactional
+@ExtendWith(SpringExtension.class)
+@Import(VacationMapper.class)
 class VacationServiceTests {
-
-	@Autowired
-	MemberRepository memberRepository;
-	@Autowired
-	VacationService vacationRequestService;
-	@Autowired
-	AuthService authService;
-	@Autowired
+	@Mock
+	private VacationMapper vacationMapper;
+	@Mock
+	private VacationInfoRepository vacationInfoRepository;
+	@Mock
 	private VacationRequestRepository vacationRequestRepository;
-	@Autowired
+	@Mock
+	private ApprovalStepRepository approvalStepRepository;
+	@Mock
+	private MemberRepository memberRepository;
+	@Mock
 	private CodeRepository codeRepository;
+	@Mock
+	private ApprovalStepService approvalStepService;
+	@Mock
+	private VacationRequestSearchRepository vacationRequestSearchRepository;
+	@InjectMocks
+	private VacationService vacationService;
+
+	@Nested
+	class getMyVacationInfo {
+		@Test
+		void should_throwRuntimeException_when_givenNotExistMemberId() {
+			// when
+			when(memberRepository.findById(0L)).thenReturn(Optional.empty());
+
+			// then
+			assertThatThrownBy(() -> vacationService.getMyVacationInfo(0L)).isInstanceOf(RuntimeException.class);
+		}
+	}
+
+	@Nested
+	class requestVacation {
+		@Test
+		void should_throwRuntimeException_when_givenNotExistVacationInfo() {
+			// when
+			when(memberRepository.findByIdWithDeptAndLeader(0L)).thenReturn(Optional.empty());
+
+			// then
+			assertThatThrownBy(() -> vacationService.requestVacation(0L, null)).isInstanceOf(RuntimeException.class);
+		}
+
+		@Test
+		void should_throwBadRequestException_when_givenInvalidFromAndTo() {
+			// given
+			Member member = MemberMother.member();
+			LocalDateTime from = LocalDateTime.now().plusDays(1);
+			LocalDateTime to = LocalDateTime.now();
+			VacationCreateRequestDto vacationCreateRequestDto = new VacationCreateRequestDto(from, to, "empty",
+				"empty");
+
+			// when
+			when(memberRepository.findByIdWithDeptAndLeader(0L)).thenReturn(Optional.of(member));
+			when(vacationRequestRepository.countInRangeFromBetweenToBy(member.getId(),
+				from, to)).thenReturn(1l);
+
+			// then
+			assertThatThrownBy(
+				() -> vacationService.requestVacation(member.getId(), vacationCreateRequestDto)).isInstanceOf(
+				BadRequestException.class);
+		}
+
+		@Test
+		void should_throwNotFoundException_when_givenInvalidMemberIdAndVacationInfoType() {
+			// given
+			Member member = MemberMother.member();
+			LocalDateTime from = LocalDateTime.now().plusDays(1);
+			LocalDateTime to = LocalDateTime.now();
+			String vacationType = "vacationType";
+			VacationCreateRequestDto vacationCreateRequestDto = new VacationCreateRequestDto(from, to, "reason",
+				vacationType);
+
+			// when
+			when(memberRepository.findByIdWithDeptAndLeader(0L)).thenReturn(Optional.of(member));
+			when(vacationRequestRepository.countInRangeFromBetweenToBy(member.getId(),
+				vacationCreateRequestDto.getFrom(), vacationCreateRequestDto.getTo())).thenReturn(0l);
+			when(vacationRequestRepository.calculateRequestedVacationDays(from, to, vacationType)).thenReturn(0d);
+			when(vacationInfoRepository.findActualRemainingVacationDays(member.getId(), vacationType)).thenReturn(
+				Optional.empty());
+
+			// then
+			assertThatThrownBy(
+				() -> vacationService.requestVacation(member.getId(), vacationCreateRequestDto)).isInstanceOf(
+				NotFoundException.class);
+		}
+
+		@Test
+		void should_throwBadRequestException_when_actualRemainCountLessThanRequestDays() {
+			// given
+			Member member = MemberMother.member();
+			LocalDateTime from = LocalDateTime.now().plusDays(1);
+			LocalDateTime to = LocalDateTime.now();
+			String vacationType = "vacationType";
+			VacationCreateRequestDto vacationCreateRequestDto = new VacationCreateRequestDto(from, to, "reason",
+				vacationType);
+
+			// when
+			when(memberRepository.findByIdWithDeptAndLeader(0L)).thenReturn(Optional.of(member));
+			when(vacationRequestRepository.countInRangeFromBetweenToBy(member.getId(),
+				vacationCreateRequestDto.getFrom(), vacationCreateRequestDto.getTo())).thenReturn(0l);
+			when(vacationRequestRepository.calculateRequestedVacationDays(from, to, vacationType)).thenReturn(5d);
+			when(vacationInfoRepository.findActualRemainingVacationDays(member.getId(), vacationType)).thenReturn(
+				Optional.of(0d));
+
+			// then
+			assertThatThrownBy(
+				() -> vacationService.requestVacation(member.getId(), vacationCreateRequestDto)).isInstanceOf(
+				BadRequestException.class);
+		}
+
+		@Test
+		void should_throwRuntimeException_when_givenInvalidGroupCodeAndCode() {
+			// given
+			Member member = MemberMother.member();
+			LocalDateTime from = LocalDateTime.now().plusDays(1);
+			LocalDateTime to = LocalDateTime.now();
+			String vacationType = "vacationType";
+			VacationCreateRequestDto vacationCreateRequestDto = new VacationCreateRequestDto(from, to, "reason",
+				vacationType);
+
+			// when
+			when(memberRepository.findByIdWithDeptAndLeader(0L)).thenReturn(Optional.of(member));
+			when(vacationRequestRepository.countInRangeFromBetweenToBy(member.getId(),
+				vacationCreateRequestDto.getFrom(), vacationCreateRequestDto.getTo())).thenReturn(0l);
+			when(vacationRequestRepository.calculateRequestedVacationDays(from, to, vacationType)).thenReturn(0d);
+			when(vacationInfoRepository.findActualRemainingVacationDays(member.getId(), vacationType)).thenReturn(
+				Optional.of(1d));
+
+			// then
+			assertThatThrownBy(
+				() -> vacationService.requestVacation(member.getId(), vacationCreateRequestDto)).isInstanceOf(
+				BadRequestException.class);
+		}
+
+	}
 
 	@Test
-	@DisplayName("캘린더 결과 테스트")
-	void select_calendar_test() throws Exception {
+	void should_saveVacation_when_givenValidMemberIdAndVacationCreateRequestDto() {
+		// given
+		Member member = MemberMother.member();
+		Member leader = MemberMother.member();
+		member.getDept().appointLeader(leader);
 
-		MemberSignUpRequest memberSignUpRequest = new MemberSignUpRequest(
-			"홍길동",
-			"hong@naver.com",
-			1L,
-			"01",
-			LocalDateTime.now(),
-			"1990-01-01",
-			"password1234");
+		LocalDateTime from = LocalDateTime.now();
+		LocalDateTime to = from.plusDays(2);
+		VacationCreateRequestDto vacationCreateRequestDto = new VacationCreateRequestDto(from, to, "reason",
+			"vacationType");
 
-		authService.signUp(memberSignUpRequest);
+		// when
+		when(memberRepository.findByIdWithDeptAndLeader(member.getId())).thenReturn(Optional.of(member));
+		when(vacationRequestRepository.countInRangeFromBetweenToBy(member.getId(), from, to)).thenReturn(0l);
+		when(vacationRequestRepository.calculateRequestedVacationDays(from, to,
+			vacationCreateRequestDto.getVacationType())).thenReturn((double)Duration.between(from, to).toDays());
+		when(vacationInfoRepository.findActualRemainingVacationDays(member.getId(),
+			vacationCreateRequestDto.getVacationType())).thenReturn(Optional.of(30d));
+		when(codeRepository.findByGroupCodeAndCode("VACATION_TYPE",
+			vacationCreateRequestDto.getVacationType())).thenReturn(Optional.empty());
 
-		Member savedMember = memberRepository.findByEmail("hong@naver.com")
-			.orElseThrow(() -> new RuntimeException("회원 저장 실패 "));
+		// then
+		assertThatThrownBy(
+			() -> vacationService.requestVacation(member.getId(), vacationCreateRequestDto)).isInstanceOf(
+			RuntimeException.class);
+	}
 
-		Code vacationType = codeRepository.findByGroupCodeAndCode("VACATION_TYPE", "01")
-			.orElseThrow(() -> new RuntimeException("잘못된 휴가 유형입니다."));
+	@Nested
+	class GetVacationRequestList {
 
-		VacationRequest vacationRequest = new VacationRequest(savedMember, LocalDateTime.now(), LocalDateTime.now(),
-			"사유", vacationType,
-			VacationRequestStatus.APPROVED, 1);
+		@Test
+		void should_returnEmptyResult_when_findIdPageIsEmpty() {
+			// given
+			Member member = MemberMother.member();
 
-		for (int i = 0; i < 5; i++) {
-			vacationRequestRepository.save(vacationRequest);
+			// when
+			when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+			when(vacationRequestRepository.findIdsByRequesterIdPaging(anyLong(), any(Pageable.class))).thenReturn(
+				Page.empty());
+
+			// then
+			VacationListResponseDto result = vacationService.getVacationRequestList(member.getId(), 0);
+			assertThat(result.getContent()).isEmpty();
+			assertThat(result.getPageNumber()).isEqualTo(0);
 		}
+	}
 
-		List<VacationRequestCalendarResponse> vacationRequestCalendarResponses = vacationRequestService.selectVacationCalendar(
-			"2025-05", 1L);
-
-		log.info("approvedVacationsByMonth.size() = {}", vacationRequestCalendarResponses.size());
-
-		assertThat(vacationRequestCalendarResponses.size()).isNotEqualTo(0);
-
-		for (VacationRequestCalendarResponse vacationRequestCalendarResponse : vacationRequestCalendarResponses) {
-			log.info("vacationRequestCalendarResponse = {}", vacationRequestCalendarResponse);
-		}
+	@Nested
+	class UpdateVacationRequest {
+		/**
+		 * requestVacation 메서드와 겹치는게 많아서 리팩토링 후 테스트 추가가 필요해 보임
+		 * @author gunwoong
+		 */
 	}
 
 }
