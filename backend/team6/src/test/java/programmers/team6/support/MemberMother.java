41d1368a7d255e261178b7e6b8785be865bb820a
민경준 <wsx8028@gmail.com>
2025-06-13 09:31:36 +0900

    test: ApprovalStepService 테스트 코드 작성 (#27)
    Co-authored-by: Gunwoong cho <80460636+gunwoong1630@users.noreply.github.com>

--- backend/team6/src/test/java/programmers/team6/support/MemberMother.java
+++ backend/team6/src/test/java/programmers/team6/support/MemberMother.java
@@ -20,7 +20,18 @@ public class MemberMother {
 			.build();
 	}
 
-	public static Member member() {
+	public static Member withIdAndDeptName(Long id, String deptName) {
+		return MemberStub.subBuilder()
+			.id(id)
+			.name("testMember" + id)
+			.dept(new Dept(deptName, null))
+			.position(PositionMother.employee())
+			.joinDate(LocalDateTime.of(2025, 6, 12, 10, 11))
+			.role(Role.USER)
+			.build();
+  }
+	
+  public static Member member() {
 		return withId(0L);
 	}
 }
