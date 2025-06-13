2a50d7e99db041ba65261ffb8b819c6854d80c5f
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2025-06-13 09:06:06 +0900

    test(22) : VacationStatisticsService 테스트 작성 (#25)
    * test : 사용자 휴가 정보출력 테스트 작성
    
    * test : TargetVacationRequests 테스트 작성
    
    * refactor : fake객체 단순화
    
    * refactor : 안쓰는 의존성 제거

---
+++ backend/team6/src/test/java/programmers/team6/mock/MemberReaderFake.java
@@ -0,0 +1,28 @@
+package programmers.team6.mock;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import programmers.team6.domain.admin.dto.VacationStatisticsRequest;
+import programmers.team6.domain.admin.service.MemberReader;
+import programmers.team6.domain.admin.service.Members;
+import programmers.team6.domain.member.entity.Member;
+
+public class MemberReaderFake extends MemberReader {
+
+	private final List<Member> members;
+
+	public MemberReaderFake(Member... members) {
+		super(null, null);
+		this.members = new ArrayList<>(Arrays.asList(members));
+	}
+
+	@Override
+	public Members readHasVacationInfoMemberFrom(VacationStatisticsRequest request, Pageable pageable) {
+		return new Members(new PageImpl<>(members, pageable, members.size()));
+	}
+}
---
+++ backend/team6/src/test/java/programmers/team6/mock/VacationInfoLogReaderFake.java
@@ -0,0 +1,35 @@
+package programmers.team6.mock;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import programmers.team6.domain.admin.dto.VacationStatisticsRequest;
+import programmers.team6.domain.admin.service.VacationInfoLogReader;
+import programmers.team6.domain.admin.service.VacationInfoLogs;
+import programmers.team6.domain.vacation.entity.VacationInfoLog;
+
+public class VacationInfoLogReaderFake extends VacationInfoLogReader {
+
+	private final Map<Long, VacationInfoLog> vacationInfoLogs;
+
+	public VacationInfoLogReaderFake(VacationInfoLog... logs) {
+		super(null);
+		this.vacationInfoLogs = toMap(Arrays.asList(logs));
+	}
+
+	private static Map<Long, VacationInfoLog> toMap(List<VacationInfoLog> vacationRequests) {
+		Map<Long, VacationInfoLog> map = new HashMap<>();
+		for (VacationInfoLog infoLog : vacationRequests) {
+			map.put(infoLog.getMemberId(), infoLog);
+		}
+		return map;
+	}
+
+	@Override
+	public VacationInfoLogs lastedLogsFrom(List<Long> ids, VacationStatisticsRequest request) {
+		return new VacationInfoLogs(vacationInfoLogs);
+	}
+
+}
