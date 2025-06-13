0884f990e4762c161012ff6f15cd9a9e70c6223b
sehee <sskfro6@gmail.com>
2025-06-13 12:51:15 +0900

    test : member -> members 로 테이블명 변경으로 인한 sql 문 수정

--- backend/team6/src/test/resources/data-test.sql
+++ backend/team6/src/test/resources/data-test.sql
@@ -6,7 +6,7 @@ INSERT INTO member_info (member_info_id, birth, email, password, created_at, upd
 VALUES (1, '1995-01-01', 'leader@dev.com', '$2b$12$Yu66yyMjz2EIvQWLhvpOi.Er05Q9JfhMLLweIjuQWw6BRuPrFv5fW', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 /* password : password1234 */
 
-INSERT INTO member (member_id, name, join_date, role, dept_id, member_info_id, position_id, created_at, updated_at)
+INSERT INTO members (member_id, name, join_date, role, dept_id, member_info_id, position_id, created_at, updated_at)
 VALUES (1, '리더', CURRENT_TIMESTAMP, 'ADMIN', NULL, 1, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
