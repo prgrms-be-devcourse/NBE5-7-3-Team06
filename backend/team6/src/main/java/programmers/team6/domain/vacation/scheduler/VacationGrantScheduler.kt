05cb59474b5c67d11121523ae3fcd9661c0bed64
hunko <rhwlgns4386@naver.com>
2025-06-16 01:45:17 +0900

    migration : VacationGrantScheduler 마이그레이션

--- backend/team6/src/main/java/programmers/team6/domain/vacation/scheduler/VacationGrantScheduler.kt
+++ backend/team6/src/main/java/programmers/team6/domain/vacation/scheduler/VacationGrantScheduler.kt
@@ -1,23 +1,19 @@
-package programmers.team6.domain.vacation.scheduler;
+package programmers.team6.domain.vacation.scheduler
 
-import java.time.LocalDate;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import programmers.team6.domain.vacation.service.VacationGrantService;
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import programmers.team6.domain.vacation.service.VacationGrantService
+import java.time.LocalDate
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class VacationGrantScheduler {
+class VacationGrantScheduler(private val vacationGrantService: VacationGrantService) {
 
-	private final VacationGrantService vacationGrantService;
-
-	@Scheduled(cron = "${schedule.grant-cron}")
-	public void grantJob() {
-		vacationGrantService.grantAnnualVacations(LocalDate.now());
-	}
+    @Scheduled(cron = "\${schedule.grant-cron}")
+    fun grantJob() {
+        vacationGrantService.grantAnnualVacations(LocalDate.now())
+    }
 }
