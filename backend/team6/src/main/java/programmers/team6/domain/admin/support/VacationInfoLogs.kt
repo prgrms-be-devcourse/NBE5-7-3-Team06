2a809023159bf4415b0887bf895bd8c68ca58e22
hunko <rhwlgns4386@naver.com>
2025-06-17 10:37:39 +0900

    feat : VacationInfoLogs equals and hahscode추가

--- backend/team6/src/main/java/programmers/team6/domain/admin/support/VacationInfoLogs.kt
+++ backend/team6/src/main/java/programmers/team6/domain/admin/support/VacationInfoLogs.kt
@@ -1,9 +1,22 @@
 package programmers.team6.domain.admin.support
 
-import lombok.EqualsAndHashCode
 import programmers.team6.domain.vacation.entity.VacationInfoLog
 
-@EqualsAndHashCode
 class VacationInfoLogs(private val lastedMap: Map<Long, VacationInfoLog>) {
+
     fun findVacationInfo(id: Long): VacationInfoLog? = lastedMap[id]
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as VacationInfoLogs
+
+        return lastedMap == other.lastedMap
+    }
+
+    override fun hashCode(): Int {
+        return lastedMap.hashCode()
+    }
+
 }
